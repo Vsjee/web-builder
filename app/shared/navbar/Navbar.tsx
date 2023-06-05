a9d3cdd92c0d39e9fd2b2a88c8372638adf635d5
david hernandez <davidfelipehernandez1@gmail.com>
2023-06-04 19:21:41 -0500

    update use tailwind instead of css

--- app/shared/navbar/Navbar.tsx
+++ app/shared/navbar/Navbar.tsx
@@ -1,36 +1,40 @@
 'use client';
 import { useRouter } from 'next/navigation';
-import './navbar.css';
 import Link from 'next/link';
 
 function Navbar() {
   const router = useRouter();
 
   return (
-    <nav className='nav'>
-      <div className='nav__links'>
-        <picture className='nav__pic'>
+    <nav className='bg-slate-50 flex justify-between items-center px-8 py-2'>
+      <div className='flex items-center gap-2'>
+        <picture>
           <img
             src='../../../assets/logo.png'
             alt='logo'
-            className='pic__img'
+            width={50}
+            height={50}
+            className='cursor-pointer'
             onClick={() => router.push('/')}
           />
         </picture>
-        <ul className='links__list'>
-          <Link href='' className='list__item'>
+        <ul className=' flex gap-5 text-xs'>
+          <Link href='' className=' hover:text-blue-500 ease-in duration-300'>
             Creación
           </Link>
-          <span className='separator'>|</span>
-          <Link href='' className='list__item'>
+          <span className='text-gray-300'>|</span>
+          <Link href='' className=' hover:text-blue-500 ease-in duration-300'>
             Precios
           </Link>
-          <Link href='' className='list__item'>
+          <Link href='' className='hover:text-blue-500 ease-in duration-300'>
             Centro de ayuda
           </Link>
         </ul>
       </div>
-      <button className='nav__login' onClick={() => router.push('/pages/public/signin')}>
+      <button
+        className='text-xs py-1 px-6 border-2 text-blue-500 border-blue-500 hover:bg-blue-500 hover:text-slate-50  rounded-full ease-in duration-300'
+        onClick={() => router.push('/pages/public/signin')}
+      >
         Entrar
       </button>
     </nav>
