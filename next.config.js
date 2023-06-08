c32cbff4b35248ddffc2d1169bf28b8d80784597
david hernandez <davidfelipehernandez1@gmail.com>
2023-06-07 21:12:21 -0500

    add next redirection

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/public/home',
+        permanent: true,
+      },
+    ];
+  },
   images: {
     domains: ['images.unsplash.com'],
   }
